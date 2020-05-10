9bfa03a4d8c63342b60d038faae74d60d560aa5d
fahadahmadansari111 <abc@ABCs-MacBook-Pro.local>
2020-05-10 15:53:22 +0530

    Syntax correction

--- deck_test.go
+++ deck_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 16 {
-		t.Errorf("Expected deck length of 16, but got", lend(d))
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
 }
